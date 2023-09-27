2910cadffd281fcc8d76901e8b635209c8b8ab04
Leology0 <leology42@gmail.com>
2023-09-27 12:58:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ def main():
     file = open("baby-names-data.json")
     baby_data = json.load(file)
     file.close()
-
     # Main Menu
     loop = True
     while loop:
@@ -45,27 +44,68 @@ def getMenuSelection():
 def displayAll(baby_data):
     # Display All Baby Data
     print("\nDISPLAY ALL")
+    for i in range(len(baby_data)):
+        baby_name = baby_data[i].get("name")
+        baby_rank = baby_data[i].get("rank")
+        baby_gender = baby_data[i].get("gender")
+        print(f"{baby_name} (Rank: {baby_rank}, Gender: {baby_gender})")
 
 
 def searchGender(baby_data):
     # Dislay All Baby Names based on Gender
     print("\nSEARCH BY GENDER")
-
+    gender_selection = input("Enter a gender (Boy/Girl): ")
+    for i in range(len(baby_data)):
+        baby_name = baby_data[i].get("name")
+        baby_rank = baby_data[i].get("rank")
+        baby_gender = baby_data[i].get("gender")
+        if baby_gender == gender_selection:
+            print(f"{baby_name} (Rank: {baby_rank}, Gender: {baby_gender})")
 
 def searchRank(baby_data):
     # Display All Baby Names based on Rank
     print("\nSEARCH BY RANK")
-
+    rank_selection_minimum = input("Enter a minimum rank: ")
+    rank_selection_maximum = input("Enter a maximum rank: ")
+    baby_count = 0
+    for i in range(len(baby_data)):
+        baby_name = baby_data[i].get("name")
+        baby_rank = baby_data[i].get("rank")
+        baby_gender = baby_data[i].get("gender")
+        if baby_rank >= int(rank_selection_minimum) and baby_rank <= int(rank_selection_maximum):
+            print(f"{baby_name} (Rank: {baby_rank}, Gender: {baby_gender})")
+            baby_count += 1
+    print(f"Number of names found: {baby_count}")
 
 def searchStartLetter(baby_data):
     # Insert User Item into a Position
     print("\nSEARCH BY START LETTER")
-
+    letter_selection = input("Enter a starting letter: ")
+    baby_count = 0
+    for i in range(len(baby_data)):
+        baby_name = baby_data[i].get("name")
+        baby_rank = baby_data[i].get("rank")
+        baby_gender = baby_data[i].get("gender")
+        if baby_name[0] == letter_selection:
+            print(f"{baby_name} (Rank: {baby_rank}, Gender: {baby_gender})")
+            baby_count += 1
+    print(f"Number of names found: {baby_count}")
 
 def searchNameLength(baby_data):
     # Remove item from position
     print("\nSEARCH BY NAME LENGTH")
-
+    name_length_selection = input("Enter a name length: ")
+    baby_count = 0
+    for i in range(len(baby_data)):
+        baby_name = baby_data[i].get("name")
+        baby_rank = baby_data[i].get("rank")
+        baby_gender = baby_data[i].get("gender")
+        if len(baby_name) == int(name_length_selection):
+            print(f"{baby_name} (Rank: {baby_rank}, Gender: {baby_gender})")
+            baby_count += 1
+    print(f"Number of names found: {baby_count}")
 
 # Invoke main to begin program
 main()
+
+
